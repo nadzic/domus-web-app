bff39569aba793e2c4bd9a38b1bed18492bd4834
Nik Adzic <nik@adzic.si>
2018-07-06 14:58:23 +0200

    add login form, add basic logics for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create 
+    user = User.find(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:passowrd])
+      # code comes here
+    else
+      render 'new'
+    end  
+  end
+  
+  def destroy
   end
 end
